713f69b9b3a7a8e57176d7c8531af06aa82d163d
Briana <briana_brewster4@yahoo.com>
2022-09-01 10:03:22 -0500

    commit tree 640959b03e98a3415c06fd84f1f0e7ac46ca4098

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+function findMatching(drivers, name) {
+    const result = drivers.filter(element => element.toLowerCase().includes(name.toLowerCase()))
+    return result
+}
+
+function fuzzyMatch(drivers, string) {
+    const result = drivers.filter(element => element.startsWith(string))
+    return result
+}
+
+function matchName(drivers, string) {
+    const result = drivers.filter(element => element.name === string)
+    return result
+}
